9216b1706b5645d5cbb20876ff1c75686b7ad995
Ze-hs <thehao33@gmail.com>
2024-11-09 16:49:39 -0500

    Fixed store error

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-
-createRoot(document.getElementById('root')).render(
-    <Provider store={}>
-    <App />
-
-    </Provider >
-)
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./reducers/store.js";
+createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
