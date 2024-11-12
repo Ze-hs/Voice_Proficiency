c7619753aeed034c002e91c48e3bff03ad6dc595
Ze-hs <thehao33@gmail.com>
2024-11-12 17:41:23 -0500

    Added user reducer

--- client/src/reducers/store.js
+++ client/src/reducers/store.js
@@ -2,12 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import mediaReducer from "./mediaReducer";
 import transcriptListReducer from "./transcriptListReducer";
 import transcriptReducer from "./transcriptReducer";
+import userReducer from "./userReducer";
+
 const store = configureStore({
     reducer: {
         //URL
         media: mediaReducer,
         transcripts: transcriptListReducer,
         currentTranscript: transcriptReducer,
+        user: userReducer,
     },
 });
 
