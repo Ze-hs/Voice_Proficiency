524d506d291d7e47fe6d5793523d67dfa3129624
Ze-hs <thehao33@gmail.com>
2024-11-17 11:36:08 -0500

    Fixed mistake with PORT definition

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const app = require("./app");
 // const config = require("./utils/config");
 const utils = require("./utils/utils");
 
-app.listen(process.env.PORT || 3001, () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
     utils.info(`Server running on port ${PORT}`);
 });
